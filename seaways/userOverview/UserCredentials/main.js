fc3fec4d0e95332b24f15819ea2832a12232574a
lenomed <lenomedmega@gmail.com>
2023-07-13 14:31:55 +0100

    new page

---
+++ seaways/userOverview/UserCredentials/main.js
@@ -0,0 +1,21 @@
+// document.getElementById('order-form').addEventListener('submit', function(event) {
+//     event.preventDefault(); // Prevent form submission
+    
+//     var name = document.getElementById('name').value;
+//     var address = document.getElementById('address').value;
+//     var phone = document.getElementById('phone').value;
+//     var item = document.getElementById('item').value;
+    
+//     // Simulate sending order data to a server
+//     setTimeout(function() {
+//       var statusMessage = document.getElementById('status-message');
+//       statusMessage.textContent = 'Your order for ' + item + ' has been placed! We will deliver it to ' + address + '. Thank you, ' + name + '!';
+  
+//       // Clear the form fields
+//       document.getElementById('name').value = '';
+//       document.getElementById('address').value = '';
+//       document.getElementById('phone').value = '';
+//       document.getElementById('item').value = '';
+//     }, 2000);
+//   });
+  
